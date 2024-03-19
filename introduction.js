6cfe90def6061c78cf66be73c3921585c94ca1ca
Lucas-Buscarioli <lucas.buscarioli@gmail.com>
2024-03-19 17:03:52 -0300

    reading done

---
+++ introduction.js
@@ -0,0 +1,22 @@
+console.log('Hello, world!')
+
+let = firstName = 'Lucas'
+firstName = 'Carl' //variable changes value
+console.log(firstName)
+
+const PI = 3.14 //not allowed to reassign
+console.log(PI)
+
+let nums = [1, 2, 3]
+
+nums [0] = 10 // modifying pos 0
+console.log(nums) // [10, 2, 3]
+
+console.log(typeof 'Asabeneh') // string
+console.log(typeof 5) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
+
+// ***** DATA TYPES: https://github.com/Asabeneh/30-Days-Of-JavaScript/blob/master/02_Day_Data_types/02_day_data_types.md *******
+
